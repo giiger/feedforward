5638cec556e0e2f4cbdfd3b34f1fcb707cd8b78e
paul <pasimko13@gmail.com>
2020-01-13 11:14:37 -0800

    Porting to c#

--- Assets/Scripts/math.cs
+++ Assets/Scripts/math.cs
@@ -1,21 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 
 public static class math
 {
     public static double E = 2.7182818284590451;
 
-    public static List<double> arrSub(double[] a, double[b])
+    public static List<double> arrSub(List<double> a, List<double> b)
     {
-        double[] c;
-        for (int i = 0; i < a.Length; i ++)
+        List<double> c = new List<double>();
+        for (int i = 0; i < a.Count; i ++)
         {
-            c.ad
+            c.Add(a[i] - b[i]);
         }
+        return c;
+    }
+    public static List<double> arrAdd(List<double> a, List<double> b)
+    {
+        List<double> c = new List<double>();
+        for (int i = 0; i < a.Count; i++)
+        {
+            c.Add(a[i] + b[i]);
+        }
+        return c;
+    }
+    public static List<double> arrPow(List<double> a, double power)
+    {
+        List<double> c = new List<double>();
+        for (int i = 0; i < a.Count; i++)
+        {
+            c.Add(Math.Pow(a[i], power));
+        }
+        return c;
+    }
+    public static List<double> arrMean(List<double> a, double power)
+    {
+        List<double> c = new List<double>();
+        for (int i = 0; i < a.Count; i++)
+        {
+            c.Add(Math.Pow(a[i], power));
+        }
+        return c;
     }
 
-
+    public static double arrDot(List<double> a, List<double> b)
+    {
+        double c = 0;
+        for (int i = 0; i < a.Count; i++)
+        {
+            c +=(a[i] * b[i]);
+        }
+        return c;
+    }
 
     public static double sigmoid(double x) {
         // Sigmoid activation function: f(x) = 1 / (1 + e^(-x))
@@ -28,9 +65,9 @@ public static class math
         double fx = sigmoid(x);
         return fx * (1 - fx);
     }
-    public static double[] mse_loss(double[] y_true, double[] y_pred)
+    public static double mse_loss(List<double> y_true, List<double> y_pred)
     {
         // y_true and y_pred are numpy arrays of the same length.
-        return Math.Pow(y_true - y_pred, 2).mean();
+        return arrPow(arrSub(y_true, y_pred), 2).Average();
     }
 }
