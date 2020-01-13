fa328625b7fd39e4c6eafc0b7994ec374c0356fc
Alex Giger <a13xgiger@gmail.com>
2020-01-13 10:05:24 -0800

    added scenes

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
