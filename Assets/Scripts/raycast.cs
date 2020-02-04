5b544316b098dbb9b237ad809a084ab411beee33
Alex Giger <a13xgiger@gmail.com>
2020-02-04 11:24:34 -0800

    Raycasting

---
+++ Assets/Scripts/raycast.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour
+{
+    void Update()
+    {
+        RaycastHit hit;
+        float theDistance;
+
+        Vector3 up = transform.TransformDirection(Vector3.up) * 10;
+        Debug.DrawRay(transform.position, up, Color.green);
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up),out hit, Mathf.Infinity))
+        {
+            theDistance = hit.distance;
+            Debug.Log("hit");
+            Debug.Log(theDistance);
+        }
+    }
+}
