c3bf8428b8afde6c9eccc1f8e95a91d535d76903
Alex Giger <a13xgiger@gmail.com>
2020-02-06 11:21:59 -0800

    Car movement and edge detection

---
+++ Assets/Scripts/TestMove.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+    public GameObject car;
+    //change velocity to vector
+    float velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            Vector2 position = car.transform.position;
+            velocity += 0.001f;
+            position.y += velocity;
+            position.x -= 0.03f;
+            car.transform.position = position;
+        }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            Vector2 position = car.transform.position;
+            velocity += 0.001f;
+            position.y += velocity;
+            position.x += 0.03f;
+            car.transform.position = position;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            Vector2 position = car.transform.position;
+            velocity -= 0.001f;
+            position.y += velocity;
+            position.x -= 0.03f;
+            car.transform.position = position;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            Vector2 position = car.transform.position;
+            velocity -= 0.001f;
+            position.y += velocity;
+            position.x += 0.03f;
+            car.transform.position = position;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            Vector2 position = car.transform.position;
+            velocity += 0.001f;
+            position.y += velocity;
+            car.transform.position = position;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 position = car.transform.position;
+            position.x -= 0.03f;
+            car.transform.position = position;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Vector2 position = car.transform.position;
+            velocity -= 0.001f;
+            position.y += velocity;
+            car.transform.position = position;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Vector2 position = car.transform.position;
+            position.x += 0.03f;
+            car.transform.position = position;
+        }
+        else if(velocity != 0)
+        {
+            Vector2 position = car.transform.position;
+            position.y += velocity;
+            velocity -= 0.002f;
+            car.transform.position = position;
+        }
+    }
+}
--- Assets/Scripts/raycast.cs
+++ Assets/Scripts/raycast.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class raycast : MonoBehaviour
 {
-    void Update()
+    void FixedUpdate()
     {
-        RaycastHit hit;
-        float theDistance;
-
-        Vector3 up = transform.TransformDirection(Vector3.up) * 10;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
+        RaycastHit2D hitR = Physics2D.Raycast(transform.position, Vector2.right);
+        RaycastHit2D hitL = Physics2D.Raycast(transform.position, Vector2.left);
+        Vector2 up = transform.TransformDirection(Vector2.up) * 10;
+        Vector2 right = transform.TransformDirection(Vector2.right) * 10;
+        Vector2 left = transform.TransformDirection(Vector2.left) * 10;
         Debug.DrawRay(transform.position, up, Color.green);
+        Debug.DrawRay(transform.position, right, Color.green);
+        Debug.DrawRay(transform.position, left, Color.green);
 
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up),out hit, Mathf.Infinity))
+        if (hit.collider != null)
         {
-            theDistance = hit.distance;
-            Debug.Log("hit");
-            Debug.Log(theDistance);
+            float distance = Mathf.Abs(hit.point.y - transform.position.y);
+            Debug.Log(distance);
+            if(distance <= 0.1f)
+            {
+                Debug.Log("Collision!");
+                GetComponent<TestMove>().enabled = false;
+            }
+        }
+        if (hitR.collider != null)
+        {
+            float distanceR = Mathf.Abs(hitR.point.x - transform.position.x);
+            Debug.Log(distanceR);
+            if (distanceR <= 0.1f)
+            {
+                Debug.Log("Collision!");
+                GetComponent<TestMove>().enabled = false;
+            }
+        }
+        if (hitL.collider != null)
+        {
+            float distanceL = Mathf.Abs(hitL.point.x - transform.position.x);
+            Debug.Log(distanceL);
+            if (distanceL <= 0.1f)
+            {
+                Debug.Log("Collision!");
+                GetComponent<TestMove>().enabled = false;
+            }
         }
     }
 }
