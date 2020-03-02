f3ed354c3275d999f6f049b2472bb077688ec7d8
Michael <mschuff0881@gmail.com>
2020-03-02 15:15:49 -0800

    Now able to edit number of raycasts. Found issues with colliders

--- Assets/Scripts/raycast.cs
+++ Assets/Scripts/raycast.cs
@@ -5,97 +5,99 @@ using UnityEngine.UI;
 
 public class raycast : MonoBehaviour
 {
+    public int FrontRaycastLength = 2, SideRaycastLength = 1, numOfRays = 3, startDirection = 80;
+
+    private Vector2[] vectors;
+
+    private RaycastHit2D[] hits;
+
+    private int rotateBy;
+
+    void Start() {
+        vectors = new Vector2[numOfRays];
+        hits = new RaycastHit2D[numOfRays];
+
+        rotateBy = (180 - 2 * (90 - startDirection)) / (numOfRays - 1);
+        
+        
+    }
+
     void FixedUpdate()
     {
-        int currentCheckpoint = 0;
-        bool cleared = false;
-        int clearedOnce = 0;
-        Vector2 rDir = Quaternion.AngleAxis(25.0f, Vector3.forward ) * Vector2.up * 3;
-        Vector2 lDir = Quaternion.AngleAxis(-25.0f, Vector3.forward ) * Vector2.up * 3;
-        Vector2 up = transform.TransformDirection(Vector2.up) * 10;
-        Vector2 right = transform.TransformDirection(rDir);
-        Vector2 left = transform.TransformDirection(lDir);
+        //int currentCheckpoint = 0;
+        //bool cleared = false;
+        //int clearedOnce = 0;
+        if (numOfRays % 2 == 1) {
+            for (int i = 0; i < numOfRays && i >= 0; i++) {
+                vectors[i] = Quaternion.Euler(0, 0, transform.rotation.z + i * rotateBy - startDirection) * transform.up * FrontRaycastLength;
+            }
+        }
+        for (int i = 0; i < numOfRays; i++) {
+            hits[i] = Physics2D.Raycast(transform.position, vectors[i]);
+            if (hits[i].collider.gameObject.name == "Road Outline(outer)" || hits[i].collider.gameObject.name == "Road Outline(inner)") {
+                print("Hit! Ray: " + (i + 1).ToString() + " Dist: " + hits[i].distance.ToString());
+                Debug.DrawRay(transform.position, vectors[i], Color.red);
+            } else {
+                Debug.DrawRay(transform.position, vectors[i], Color.green);
+            }
+        }
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
-        RaycastHit2D hitR = Physics2D.Raycast(transform.position, Vector2.right);
-        RaycastHit2D hitL = Physics2D.Raycast(transform.position, Vector2.left);
-        
-        Debug.DrawRay(transform.position, up, Color.green);
-        Debug.DrawRay(transform.position, left, Color.green);
-        Debug.DrawRay(transform.position, right, Color.green);
 
-        float distance = Mathf.Abs(hit.point.y - transform.position.y);
-        List<string> checkpoints = new List<string> { "checkpoint 1", "checkpoint 2", "checkpoint 3", "checkpoint 4", "checkpoint 5", "checkpoint 6", "checkpoint 7", "checkpoint 8", "checkpoint 9", "checkpoint 10", "checkpoint 11", "checkpoint 12", "checkpoint 13", "checkpoint 14", "checkpoint 15", "checkpoint 16", "checkpoint 17", "checkpoint 18", "checkpoint 19", "checkpoint 20", "checkpoint 21", "checkpoint 22", "checkpoint 23" };
+        //float distance = Mathf.Abs(hitF.point.y - transform.position.y);
 
-        foreach (string chpnt in checkpoints)
-        {
-            if (hit.collider.name.Equals(chpnt))
-            {
-                if (distance <= 0.25f)
-                {
-                    
-                    cleared = true;
-                    if (cleared == true && clearedOnce == 0)
-                    {
-                        currentCheckpoint += 1;
-                    }
-                    
-                    Debug.Log("cleared" + hit.collider.name);
-                    clearedOnce += 1;
-                }
-                cleared = false;
-                break;
-            }   
-        }
-        if (hit.collider != null && hit.collider.name.Equals(checkpoints))
-        {
-            
-            
-            
-            if(distance <= 0.15f)
-            {
-                Debug.Log("Collision!");
-                GetComponent<TestMove>().enabled = false;
-                Destroy(this.gameObject);
-            }
-            else if (distance <= 1f)
-            {
-                Debug.DrawRay(transform.position, up, Color.yellow);
-            }
-        }
-        if (hitR.collider != null)
-        {
-            float distanceR = Mathf.Abs(hitR.point.x - transform.position.x);
-            
-            if (distanceR <= 0.15f)
-            {
-                Debug.Log("Collision!");
-                GetComponent<TestMove>().enabled = false;
-                Destroy(this.gameObject);
-            }
-            else if (distanceR <= 1f)
-            {
-                Debug.DrawRay(transform.position, right, Color.yellow);
-            }
-        }
-        if (hitL.collider != null)
-        {
-            float distanceL = Mathf.Abs(hitL.point.x - transform.position.x);
-            
-            if (distanceL <= 0.15f)
-            {
-                Debug.Log("Collision!");
-                GetComponent<TestMove>().enabled = false;
-                Destroy(this.gameObject);
-            }
-            else if(distanceL <= 1f)
-            {
-                Debug.DrawRay(transform.position, left, Color.yellow);
-            }
-        }
-        
+        //if (hitF.collider != null && hitF.collider.name.Equals(checkpoints)) {
+        //    if (distance <= 0.15f) {
+        //        Debug.Log("Collision!");
+        //        GetComponent<TestMove>().enabled = false;
+        //        Destroy(this.gameObject);
+        //    } else if (distance <= 1f) {
+        //        Debug.DrawRay(transform.position, up, Color.yellow);
+        //    }
+        //}
+        //if (hitR.collider != null) {
+        //    float distanceR = Mathf.Abs(hitR.point.x - transform.position.x);
+
+        //    if (distanceR <= 0.15f) {
+        //        Debug.Log("Collision!");
+        //        GetComponent<TestMove>().enabled = false;
+        //        Destroy(this.gameObject);
+        //    } else if (distanceR <= 1f) {
+        //        Debug.DrawRay(transform.position, right, Color.yellow);
+        //    }
+        //}
+        //if (hitL.collider != null) {
+        //    float distanceL = Mathf.Abs(hitL.point.x - transform.position.x);
+
+        //    if (distanceL <= 0.15f) {
+        //        Debug.Log("Collision!");
+        //        GetComponent<TestMove>().enabled = false;
+        //        Destroy(this.gameObject);
+        //    } else if (distanceL <= 1f) {
+        //        Debug.DrawRay(transform.position, left, Color.yellow);
+        //    }
+        //}
+
     }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-    }
+    //List<string> checkpoints = new List<string> { "checkpoint 1", "checkpoint 2", "checkpoint 3", "checkpoint 4", "checkpoint 5", "checkpoint 6", "checkpoint 7", "checkpoint 8", "checkpoint 9", "checkpoint 10", "checkpoint 11", "checkpoint 12", "checkpoint 13", "checkpoint 14", "checkpoint 15", "checkpoint 16", "checkpoint 17", "checkpoint 18", "checkpoint 19", "checkpoint 20", "checkpoint 21", "checkpoint 22", "checkpoint 23" };
+
+    //foreach (string chpnt in checkpoints)
+    //{
+    //    if (hit.collider.name.Equals(chpnt))
+    //    {
+    //        if (distance <= 0.25f)
+    //        {
+
+    //            cleared = true;
+    //            if (cleared == true && clearedOnce == 0)
+    //            {
+    //                currentCheckpoint += 1;
+    //            }
+
+    //            Debug.Log("cleared" + hit.collider.name);
+    //            clearedOnce += 1;
+    //        }
+    //        cleared = false;
+    //        break;
+    //    }   
+    //}
 }
