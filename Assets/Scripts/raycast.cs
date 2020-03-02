f995b4ff0334719e04c41bdfeb7df6421d565573
Alex Giger <a13xgiger@gmail.com>
2020-03-02 11:25:48 -0800

    checkpoint system sucks :(

--- Assets/Scripts/raycast.cs
+++ Assets/Scripts/raycast.cs
@@ -7,6 +7,9 @@ public class raycast : MonoBehaviour
 {
     void FixedUpdate()
     {
+        int currentCheckpoint = 0;
+        bool cleared = false;
+        int clearedOnce = 0;
         Vector2 rDir = Quaternion.AngleAxis(25.0f, Vector3.forward ) * Vector2.up * 3;
         Vector2 lDir = Quaternion.AngleAxis(-25.0f, Vector3.forward ) * Vector2.up * 3;
         Vector2 up = transform.TransformDirection(Vector2.up) * 10;
@@ -24,15 +27,23 @@ public class raycast : MonoBehaviour
         float distance = Mathf.Abs(hit.point.y - transform.position.y);
         List<string> checkpoints = new List<string> { "checkpoint 1", "checkpoint 2", "checkpoint 3", "checkpoint 4", "checkpoint 5", "checkpoint 6", "checkpoint 7", "checkpoint 8", "checkpoint 9", "checkpoint 10", "checkpoint 11", "checkpoint 12", "checkpoint 13", "checkpoint 14", "checkpoint 15", "checkpoint 16", "checkpoint 17", "checkpoint 18", "checkpoint 19", "checkpoint 20", "checkpoint 21", "checkpoint 22", "checkpoint 23" };
 
-        foreach (var chpnt in checkpoints)
+        foreach (string chpnt in checkpoints)
         {
             if (hit.collider.name.Equals(chpnt))
             {
                 if (distance <= 0.25f)
                 {
+                    
+                    cleared = true;
+                    if (cleared == true && clearedOnce == 0)
+                    {
+                        currentCheckpoint += 1;
+                    }
+                    
                     Debug.Log("cleared" + hit.collider.name);
+                    clearedOnce += 1;
                 }
-                Debug.Log("hit " + chpnt);
+                cleared = false;
                 break;
             }   
         }
@@ -41,7 +52,7 @@ public class raycast : MonoBehaviour
             
             
             
-            if(distance <= 0.25f)
+            if(distance <= 0.15f)
             {
                 Debug.Log("Collision!");
                 GetComponent<TestMove>().enabled = false;
@@ -56,7 +67,7 @@ public class raycast : MonoBehaviour
         {
             float distanceR = Mathf.Abs(hitR.point.x - transform.position.x);
             
-            if (distanceR <= 0.25f)
+            if (distanceR <= 0.15f)
             {
                 Debug.Log("Collision!");
                 GetComponent<TestMove>().enabled = false;
@@ -71,7 +82,7 @@ public class raycast : MonoBehaviour
         {
             float distanceL = Mathf.Abs(hitL.point.x - transform.position.x);
             
-            if (distanceL <= 0.25f)
+            if (distanceL <= 0.15f)
             {
                 Debug.Log("Collision!");
                 GetComponent<TestMove>().enabled = false;
