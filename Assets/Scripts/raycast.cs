895a41fd17191e807c3288d3933105dc76b97277
Alex Giger <a13xgiger@gmail.com>
2020-03-11 11:23:38 -0700

    checkpoints function
    need to make sure you cant roll over same checkpoint

--- Assets/Scripts/raycast.cs
+++ Assets/Scripts/raycast.cs
@@ -12,8 +12,15 @@ public class raycast : MonoBehaviour
     private RaycastHit2D[] hits;
 
     private float rotateBy;
+    
 
-    void Start() {
+    private int clearedCheckpoints = 0;
+
+    private List<string> previousCollision = new List<string>();
+
+    void Start()
+    {
+
         vectors = new Vector2[(int) numOfRays];
         hits = new RaycastHit2D[(int) numOfRays];
 
@@ -22,55 +29,80 @@ public class raycast : MonoBehaviour
 
     void FixedUpdate()
     {
+        
         //int currentCheckpoint = 0;
         //bool cleared = false;
         //int clearedOnce = 0;
-        if (numOfRays % 2 == 1) {
-            for (int i = 0; i < numOfRays; i++) {
+        if (numOfRays % 2 == 1)
+        {
+            for (int i = 0; i < numOfRays; i++)
+            {
 
-                if (i < numOfRays / 2) {
-                    vectors[i] = Quaternion.Euler(0, 0, transform.rotation.z + i * rotateBy - startDirection) * transform.up * (SideRaycastLength + i * (FrontRaycastLength - SideRaycastLength) / ((int) numOfRays / 2));
-                } else {
-                    vectors[i] = Quaternion.Euler(0, 0, transform.rotation.z + i * rotateBy - startDirection) * transform.up * (FrontRaycastLength + (((int) numOfRays / 2) - i) * (FrontRaycastLength - SideRaycastLength) / ((int)numOfRays / 2));
+                if (i < numOfRays / 2)
+                {
+                    vectors[i] = Quaternion.Euler(0, 0, transform.rotation.z + i * rotateBy - startDirection) * transform.up * (SideRaycastLength + i * (FrontRaycastLength - SideRaycastLength) / ((int)numOfRays / 2));
+                }
+                else
+                {
+                    vectors[i] = Quaternion.Euler(0, 0, transform.rotation.z + i * rotateBy - startDirection) * transform.up * (FrontRaycastLength + (((int)numOfRays / 2) - i) * (FrontRaycastLength - SideRaycastLength) / ((int)numOfRays / 2));
+                }
 
-    
 
 
-        //float distance = Mathf.Abs(hitF.point.y - transform.position.y);
+                //float distance = Mathf.Abs(hitF.point.y - transform.position.y);
 
-        //if (hitF.collider != null && hitF.collider.name.Equals(checkpoints)) {
-        //    if (distance <= 0.15f) {
-        //        Debug.Log("Collision!");
-        //        GetComponent<TestMove>().enabled = false;
-        //        Destroy(this.gameObject);
-        //    } else if (distance <= 1f) {
-        //        Debug.DrawRay(transform.position, up, Color.yellow);
-        //    }
-        //}
-        //if (hitR.collider != null) {
-        //    float distanceR = Mathf.Abs(hitR.point.x - transform.position.x);
+                //if (hitF.collider != null && hitF.collider.name.Equals(checkpoints)) {
+                //    if (distance <= 0.15f) {
+                //        Debug.Log("Collision!");
+                //        GetComponent<TestMove>().enabled = false;
+                //        Destroy(this.gameObject);
+                //    } else if (distance <= 1f) {
+                //        Debug.DrawRay(transform.position, up, Color.yellow);
+                //    }
+                //}
+                //if (hitR.collider != null) {
+                //    float distanceR = Mathf.Abs(hitR.point.x - transform.position.x);
 
-        //    if (distanceR <= 0.15f) {
-        //        Debug.Log("Collision!");
-        //        GetComponent<TestMove>().enabled = false;
-        //        Destroy(this.gameObject);
-        //    } else if (distanceR <= 1f) {
-        //        Debug.DrawRay(transform.position, right, Color.yellow);
-        //    }
-        //}
-        //if (hitL.collider != null) {
-        //    float distanceL = Mathf.Abs(hitL.point.x - transform.position.x);
+                //    if (distanceR <= 0.15f) {
+                //        Debug.Log("Collision!");
+                //        GetComponent<TestMove>().enabled = false;
+                //        Destroy(this.gameObject);
+                //    } else if (distanceR <= 1f) {
+                //        Debug.DrawRay(transform.position, right, Color.yellow);
+                //    }
+                //}
+                //if (hitL.collider != null) {
+                //    float distanceL = Mathf.Abs(hitL.point.x - transform.position.x);
 
-        //    if (distanceL <= 0.15f) {
-        //        Debug.Log("Collision!");
-        //        GetComponent<TestMove>().enabled = false;
-        //        Destroy(this.gameObject);
-        //    } else if (distanceL <= 1f) {
-        //        Debug.DrawRay(transform.position, left, Color.yellow);
-        //    }
-        //}
+                //    if (distanceL <= 0.15f) {
+                //        Debug.Log("Collision!");
+                //        GetComponent<TestMove>().enabled = false;
+                //        Destroy(this.gameObject);
+                //    } else if (distanceL <= 1f) {
+                //        Debug.DrawRay(transform.position, left, Color.yellow);
+                //    }
+                //}
 
-        
+
+            }
+        }
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("cleared" + collision);
+        string col = collision.tag;
+
+        if (!previousCollision.Contains(col))
+        {
+            previousCollision.Add(col);
+            clearedCheckpoints++;
+            Debug.Log(previousCollision);
+        }
+        else if(clearedCheckpoints % 23 == 0)
+        {
+            previousCollision.Clear();
+        }
     }
     //List<string> checkpoints = new List<string> { "checkpoint 1", "checkpoint 2", "checkpoint 3", "checkpoint 4", "checkpoint 5", "checkpoint 6", "checkpoint 7", "checkpoint 8", "checkpoint 9", "checkpoint 10", "checkpoint 11", "checkpoint 12", "checkpoint 13", "checkpoint 14", "checkpoint 15", "checkpoint 16", "checkpoint 17", "checkpoint 18", "checkpoint 19", "checkpoint 20", "checkpoint 21", "checkpoint 22", "checkpoint 23" };
 
