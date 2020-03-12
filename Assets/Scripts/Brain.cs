006d0fbb3ea33a38ef384a8ac77ba6eae7a9aeca
Paul <pasimko13@gmail.com>
2020-03-11 20:56:03 -0700

    idk what i did

--- Assets/Scripts/Brain.cs
+++ Assets/Scripts/Brain.cs
@@ -3,25 +3,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Brain : MonoBehaviour
+public class Brain
 {
     int inputs = 2;
     int outputs = 3;
     // If some connections should exist before evolving, they may be initialized here
     List<List<int>> initialConnections = new List<List<int>> {
-        new List<int> {0,2},
-        new List<int> {1,3},
-        new List<int> {1,4}
+
     };
     Genome genome;
 
-    void Awake() {
+    public Brain(List<List<int>> initialConnections) {
         genome = new Genome(inputs, outputs, initialConnections);
-        List<double> output = getOutput();
-        for (int i = 0; i < output.Count; i ++) {
-            Debug.Log(output[i]);
-        }
+        this.initialConnections = initialConnections;
     }
+
     List<double> getOutput() {
         List<double> output = genome.feedforward();
         return output;
---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public List<List<int>> initialConnections = new List<List<int>> {
+        new List<int> {0,2},
+        new List<int> {0,3},
+        new List<int> {1,3},
+        new List<int> {1,4}
+    };
+    void Awake() {
+        Brain brain = new Brain(initialConnections);
+    }
+
+    public List<double> getInput() {
+        return new List<double>() {
+            transform.position.x,
+            transform.position.y
+        };
+    }
+}
