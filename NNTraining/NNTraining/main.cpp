6d85ca4eca9f860ad758b631646c35b99a05532d
Michael <mschuff0881@gmail.com>
2020-04-10 17:51:19 -0700

    fixed main.cpp

--- NNTraining/NNTraining/main.cpp
+++ NNTraining/NNTraining/main.cpp
@@ -1,91 +1,158 @@
-
 //
-// Disclaimer:
-// ----------
+//  main.cpp
+//  NNTraining
 //
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resources, use the helper
-// function `resourcePath()` from ResourcePath.hpp
+//  Created by Michael Schuff on 4/10/20.
+//  Copyright © 2020 Michael Schuff. All rights reserved.
 //
 
-#include <SFML/Audio.hpp>
+#include <iostream>
+#include <vector>
+#include <cmath>
 #include <SFML/Graphics.hpp>
+#include "SplinePoint2D.hpp"
+#include "Vector2D.hpp"
+#include "MathFunctions.hpp"
+#include "Spline2D.hpp"
 
-// Here is a small helper for you! Have a look.
-#include "ResourcePath.hpp"
+using namespace std;
+using namespace sf;
 
-int main(int, char const**)
-{
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+double degrees(double);
+double radians(double);
+RectangleShape VectorLine(Vector2D, Vector2D);
+RectangleShape Line(double, double, double, double, Color);
+void DrawSpline(RenderWindow&, Spline2D, double);
 
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+int main() {
+    enum State {leftDown, rightDown, up};
+    State mouseState = up;
+    
+    double mx, my, controlPointRadius = 5;
+    int splinePointIndex = -1, controlPointIndex = -1;
+    
+    vector<double> multipliers = {1, 5};
+    
+    vector<SplinePoint2D> controlPoints = {
+        SplinePoint2D(vector<vector<double>> {{200, 200}, {50, -50}}, multipliers),
+        SplinePoint2D(vector<vector<double>> {{400, 100}, {50, 0}}, multipliers),
+        SplinePoint2D(vector<vector<double>> {{600, 200}, {50, 50}}, multipliers),
+        SplinePoint2D(vector<vector<double>> {{700, 400}, {0, 50}}, multipliers),
+        SplinePoint2D(vector<vector<double>> {{600, 600}, {-50, 50}}, multipliers),
+        SplinePoint2D(vector<vector<double>> {{400, 700}, {-50, 0}}, multipliers),
+        SplinePoint2D(vector<vector<double>> {{200, 600}, {-50, -50}}, multipliers),
+        SplinePoint2D(vector<vector<double>> {{100, 400}, {0, -50}}, multipliers)
+    };
+    
+    Spline2D spline(controlPoints, true);
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
+    RenderWindow window(VideoMode(800, 800), "SFML Window");
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
                 window.close();
             }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
+               window.close();
             }
+
         }
-
-        // Clear screen
+        
+        if (Mouse::isButtonPressed(Mouse::Left)) {
+            mouseState = leftDown;
+        } else if (Mouse::isButtonPressed(Mouse::Right)) {
+            mouseState = rightDown;
+        } else {
+            mouseState = up;
+        }
+        
+        if (mouseState != up) {
+            mx = Mouse::getPosition(window).x;
+            my = Mouse::getPosition(window).y;
+            double minDistance = controlPointRadius;
+            for (int i = 0; i < controlPoints.size(); i++) {
+                double x = 0, y = 0;
+                for (int j = 0; j < controlPoints[i].controlPoints.size(); j++) {
+                    x += controlPoints[i].GetX(j);
+                    y += controlPoints[i].GetY(j);
+                    double distFromPoint = sqrt(pow(mx-x, 2) + pow(my-y, 2));
+                    if (distFromPoint < minDistance) {
+                        minDistance = distFromPoint;
+                        splinePointIndex = i;
+                        controlPointIndex = j;
+                    }
+                }
+            }
+            if (splinePointIndex != -1) {
+                if (mouseState == leftDown) {
+                    controlPoints[splinePointIndex].SetPositionKeepLocal(mx, my, controlPointIndex);
+                } else if (mouseState == rightDown) {
+                    controlPoints[splinePointIndex].SetPosition(mx, my, controlPointIndex);
+                }
+                spline.SetControlPoints(controlPoints);
+            }
+        } else {
+            splinePointIndex = -1;
+            controlPointIndex = -1;
+        }
         window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
+        DrawSpline(window, spline, controlPointRadius);
         window.display();
     }
+    
+}
 
-    return EXIT_SUCCESS;
+void DrawSpline(RenderWindow &window, Spline2D spline, double controlPointRadius = 5.0) {
+    vector<Color> controlPointColors = {Color(255, 0, 0), Color(255, 255, 0), Color(0, 255, 0), Color(0, 255, 255), Color(0, 0, 255), Color(255, 0, 255)};
+    Vector2D last = spline.Get(0), current(0, 0);
+    double tInc = 0.001;
+    for (double t = tInc; t < 1; t+=tInc) {
+        current = spline.Get(t);
+        window.draw(VectorLine(last, current));
+        last = current;
+    }
+    
+    for (int i = 0; i < spline.controlPoints.size(); i++) {
+        double x = spline.controlPoints[i].GetX(0), y = spline.controlPoints[i].GetY(0);
+        for (int j = 0; j < spline.controlPoints[i].controlPoints.size() - 1; j++) {
+            CircleShape p(controlPointRadius);
+            p.setOrigin(controlPointRadius / 2, controlPointRadius / 2);
+            p.setFillColor(controlPointColors[j % controlPointColors.size()]);
+            p.setPosition(x, y);
+            window.draw(Line(x, y, x+spline.controlPoints[i].GetX(j+1), y+spline.controlPoints[i].GetY(j+1), Color(100, 100, 100)));
+            window.draw(p);
+            x += spline.controlPoints[i].GetX(j+1);
+            y += spline.controlPoints[i].GetY(j+1);
+            
+        }
+        CircleShape p(controlPointRadius);
+        p.setOrigin(controlPointRadius / 2, controlPointRadius / 2);
+        p.setFillColor(controlPointColors[spline.controlPoints[i].controlPoints.size() % controlPointColors.size()]);
+        p.setPosition(x, y);
+        window.draw(p);
+    }
+}
+
+RectangleShape VectorLine(Vector2D v1, Vector2D v2) {
+    return Line(v1.x, v1.y, v2.x, v2.y, Color(255, 255, 255));
+}
+
+RectangleShape Line(double x1, double y1, double x2, double y2, Color color) {
+    double x = x2 - x1;
+    double y = y2 - y1;
+    RectangleShape line(Vector2f(sqrt(x*x+y*y),1));
+    line.setOrigin(0.5, 0.5);
+    line.rotate(degrees(atan2(y,x)));
+    line.setPosition(x1, y1);
+    line.setFillColor(color);
+    return line;
+}
+
+double radians(double deg) {
+    return deg * 3.1415626535 / 180;
+}
+
+double degrees(double rad) {
+    return rad * 180 / 3.1415626535;
 }
